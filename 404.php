89e535558bc829f4eb55740de9e7309e4bcab985
Nacho Morales <moralesignaciojoaquin@gmail.com>
2018-09-25 18:43:51 -0300

    - registrar_usuario
    - Corregido el footer.
    - Añadido perfil.php
    - Añadido 404.php

---
+++ 404.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <?php require_once("stylesheets.php") ?>
+    <title></title>
+  </head>
+  <body>
+    <div class="page-missing-container container d-flex flex-column">
+      <?php include_once("header.php") ?>
+        <div class="container page-missing-text">
+          404 :(
+        </div>
+      <?php include_once("footer.php") ?>
+    </div>
+  </body>
+</html>
