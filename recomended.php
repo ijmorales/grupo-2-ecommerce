91e5c80057dfde7519608392cfe5009cc1eca826
BHaponiuk <brendahaponiuk@gmail.com>
2018-12-09 20:38:23 -0300

    Asesor online inicio

---
+++ recomended.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<div class="container-form">
+
+
+
+<div class="recomend">
+        <div class="Pregunta">
+          <h2>¿Qué necesitás protejer?</h2>
+        </div>
+        <br>
+
+        <div class="butons-profile">
+
+          <button type="button" class="btn">Empresas</button>
+          <button type="button" class="btn">Viviendas</button>
+
+        </div>
+
+
+</div>
