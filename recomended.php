40f0513935c096fbd0e9991eb2a4b6db7539994a
BHaponiuk <brendahaponiuk@gmail.com>
2018-12-09 21:39:47 -0300

    Primera pantalla del AsesorOnline

--- recomended.php
+++ recomended.php
@@ -1,21 +1,26 @@
 <link rel="stylesheet" type="text/css" href="css/styles.css">
 <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <div class="container-form">
 
 
 
 <div class="recomend">
         <div class="Pregunta">
-          <h2>¿Qué necesitás protejer?</h2>
+          <p>¿Qué necesitás protejer?</p>
         </div>
         <br>
 
-        <div class="butons-profile">
+        <div class="btn-group btn-group-justified">
+      <a href="#" class="btn btn-primary">Empresa</a>
+      <a href="#" class="btn btn-primary">Hogar</a>
 
-          <button type="button" class="btn">Empresas</button>
-          <button type="button" class="btn">Viviendas</button>
+        </div>
+        </div>
+
 
         </div>
 
